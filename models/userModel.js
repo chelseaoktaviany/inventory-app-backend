8272fc00a28854d4de15b7cdaeaa26808d41328c
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-22 15:21:57 +0700

    Updated model for users

--- models/userModel.js
+++ models/userModel.js
@@ -4,12 +4,14 @@ const validator = require('validator');
 // membuat sebuah skema
 const userSchema = new mongoose.Schema(
   {
+    userId: { type: Number, unique: true },
     username: {
       type: String,
       required: [true, 'Please provide your username'],
     },
     emailAddress: {
       type: String,
+      sparse: true,
       unique: true,
       lowercase: true,
       required: [true, 'Please provide your email address'],
@@ -17,7 +19,7 @@ const userSchema = new mongoose.Schema(
     },
     profileImage: {
       type: String,
-      default: 'default.jpeg',
+      default: 'uploads/users/default.jpeg',
     },
     otp: {
       type: String,
@@ -33,6 +35,25 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true, versionKey: false }
 );
+
+userSchema.pre('save', function (next) {
+  const doc = this;
+  if (doc.isNew) {
+    // Auto-increment userId only for new documents
+    mongoose
+      .model('User', userSchema)
+      .findOne({}, { userId: 1 }, { sort: { userId: -1 } })
+      .then((err, lastUser) => {
+        if (err) {
+          return next(err);
+        }
+        doc.userId = lastUser ? lastUser.userId + 1 : 1;
+        next();
+      });
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
