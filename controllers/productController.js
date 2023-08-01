48f90cecd96079c8a203ab52e87b67c7c8713638
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-08-01 08:40:13 +0700

    Updated product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,7 +10,6 @@ const VendorProduct = require('../models/vendorModel');
 
 // utils
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const GroupProduct = require('../models/groupModel');
 
 exports.getAllProducts = factory.getAll(
@@ -86,92 +85,3 @@ exports.deleteProduct = factory.deleteOne(
   Product,
   'Deleted data product successfully'
 );
-
-exports.getProdReports = catchAsync(async (req, res, next) => {
-  const month = parseInt(req.params.month);
-  const year = req.params.year;
-
-  if (Number.isNaN(month) || month < 1 || month > 12 || Number.isNaN(year)) {
-    return next(new AppError('Invalid month or year', 400));
-  }
-
-  const pipeline = [
-    {
-      $facet: {
-        products: [
-          {
-            $match: {
-              purchaseDate: {
-                $gte: new Date(`${year}-${month}-01`),
-                $lte: new Date(`${year}-${month}-31`),
-              },
-            },
-          },
-          {
-            $group: {
-              _id: {
-                month: { $month: '$purchaseDate' },
-                year: { $year: '$purchaseDate' },
-              },
-              numProducts: { $sum: 1 },
-              totalPrice: { $sum: '$eachPrice' },
-              numCondGood: {
-                $sum: '$conditionGood',
-              },
-              numCondBad: {
-                $sum: '$conditionBad',
-              },
-            },
-          },
-        ],
-        // productTypes: [
-        //   {
-        //     $match: {
-        //       purchaseDateProductType: {
-        //         $gte: new Date(`${year}-${month}-01`),
-        //         $lte: new Date(`${year}-${month}-31`),
-        //       },
-        //     },
-        //   },
-        //   {
-        //     $group: {
-        //       _id: {
-        //         month: { $month: '$purchaseDateProductType' },
-        //         year: { $year: '$purchaseDateProductType' },
-        //       },
-        //       numProducts: { $sum: 1 },
-        //       totalPrice: { $sum: '$eachPriceProductType' },
-        //       numCondGood: {
-        //         $sum: '$conditionGoodProductType',
-        //       },
-        //       numCondBad: {
-        //         $sum: '$conditionBadProductType',
-        //       },
-        //     },
-        //   },
-        // ],
-      },
-    },
-    {
-      $project: {
-        products: { $arrayElemAt: ['$products', 0] },
-        // productTypes: { $arrayElemAt: ['$productTypes', 0] },
-      },
-    },
-  ];
-
-  const productReports = await Product.aggregate(pipeline);
-  // const productTypeReports = await ProductType.aggregate(pipeline);
-
-  // send response
-  res.status(200).json({
-    status: 0,
-    msg: 'Retrieved data product report successfully',
-    data: productReports,
-  });
-
-  return {
-    products: productReports[0].products,
-    // productTypes: productTypeReports[0].productTypes,
-  };
-});
